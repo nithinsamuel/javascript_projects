3ea7cbc288d037676fbe9add7c19db012da5cbb2
nithinsam77 <nithinsam77@gmail.com>
2020-08-31 22:49:48 +0530

    Added controller in app.jsk

--- tricalorie-project/app.js
+++ tricalorie-project/app.js
@@ -1,1 +1,45 @@
-console.log('app.js')
+// storage controller
+
+// item controller
+const ItemCtrl = (function () {
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+  // Data Structure/State
+  const data = {
+    items: [
+      { id: 0, name: "Steak Dinner", calories: "1200" },
+      { id: 1, name: "Cookie", calories: "400" },
+      { id: 2, name: "Eggs", calories: "300" },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+  // Public Methods
+  return {
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// ui controller
+const UICtrl = (function () {
+  // Public Methods
+  return {};
+})();
+// app controller
+const AppCtrl = (function (ItemCtrl, UICtrl) {
+  console.log(ItemCtrl.logData());
+  // Public Methods
+  return {
+    init: function () {
+      console.log("Initializing App..");
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+// Initializing the App
+AppCtrl.init();
